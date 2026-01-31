ee951b8b8e2ddff1d809d99e937f08c27fb75705
user <howard40729@gmail.com>
2026-01-31 11:49:33 -0500

    fix the mml_guidance guidance.py error. use 'ppo_model_path' and rebuild. then fix the LSTM mode not found issue in the ros2 branch(mget the correct one from the mt_observation branch)

--- install/setup.zsh
+++ install/setup.zsh
@@ -21,6 +21,9 @@ _colcon_prefix_chain_zsh_source_script() {
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
 COLCON_CURRENT_PREFIX="/opt/ros/humble"
 _colcon_prefix_chain_zsh_source_script "$COLCON_CURRENT_PREFIX/local_setup.zsh"
+# setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
+COLCON_CURRENT_PREFIX="/home/user/PPO_drone_Gazebo_ROS2/src/mml_python_sim/install"
+_colcon_prefix_chain_zsh_source_script "$COLCON_CURRENT_PREFIX/local_setup.zsh"
 
 # source this prefix
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
